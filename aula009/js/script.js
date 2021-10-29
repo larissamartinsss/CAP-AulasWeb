454c19ed987ed820f935e714b9eaac27889b333b
Larissa Martins <larissamartins.job@gmail.com>
2021-10-29 13:58:04 -0400

    aula 9

---
+++ aula009/js/script.js
@@ -0,0 +1,12 @@
+function cadastrar(){
+    let frame = document.getElementById("frame");
+    frame.src="cadastrar.html"
+}
+function listar(){
+    let frame = document.getElementById("frame");
+    frame.src="listar.html"
+}
+function contato(){
+    let frame = document.getElementById("frame");
+    frame.src="contato.html"
+}
